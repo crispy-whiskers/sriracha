6b70175956f437bf0c402a4feda17ae4d9fe3b9c
stongiey <stinggyray@gmail.com>
2020-05-01 19:14:04 -0500

    add validation for index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Discord = require('discord.js');
 const bot = new Discord.Client();
 
+var debugMode = false;
+
 bot.on('ready', function (evt) {
 	bot.user.setStatus('available');
 	bot.user.setPresence({
@@ -16,7 +18,7 @@ function clean(clothing){
 }
 
 /**
- * Cleans the matches returned in a flag regex match.
+ * Cleans the matches returned in a regex matchAll.
  * @param {Iterator} laundry 
  */
 function laundromat(laundry){
@@ -32,8 +34,22 @@ function laundromat(laundry){
     return receipt;
 }
 
+/**
+ * Validates args to make sure there are no falsy values.
+ * @param  {...any} args
+ */
+function validate(message, ...args){
+    for(var arg in args){
+        if(!args) {
+            message.channel.send('Invalid command! Make sure all required parameters are present.')
+            return false;
+        }
+    }
+    return true;
+}
 
-bot.on('message', function (message) {
+
+bot.on('message', function (message){
 	if (message.author.bot) return;
 	if (message.author.tag === 'catto#6269' || message.author.tag === 'Stinggyray#1000') {
 		if (message.content.match('^[Ss]auce stop')) {
@@ -43,32 +59,61 @@ bot.on('message', function (message) {
 		}
     }
     
-    let args = message.content.match(/^(?:[Ss]a[au]ce)\s+(?<command>move|add|list|delete|feature|random|lc|help|stats|update)?\s*(?:(?<listId>\d)#(?<entryId>\d+)(?:\s+(?<destId>\d)?))?\s*(?<flags>.*)$/)
+    //handle debug mode logic
+    let args = message.content.match(debugMode ? 
+        /^(?:[Ss]aace)\s+(?<command>move|add|list|delete|feature|random|lc|help|stats|update)?\s*(?:(?<listId>\d)(?:#(?<entryId>\d+))?(?:\s+(?<destId>\d)?))?\s*(?<flags>.*)$/ : 
+        /^(?:[Ss]auce)\s+(?<command>move|add|list|delete|feature|random|lc|help|stats|update)?\s*(?:(?<listId>\d)(?:#(?<entryId>\d+))?(?:\s+(?<destId>\d)?))?\s*(?<flags>.*)$/)
+
     if(!args?.groups)
         return;
     console.log(args);
     
     let cmd = args.groups?.command ?? 'edit';
 
-    let flags = args.groups?.flags.matchAll(/-(a|t|l|w|p|tr|pg)\s+([^-]+)/g);
+    let flags = args.groups?.flags.matchAll(/-(a|t|l|w|p|tr|pg|q|qa)\s+([^-]+)/g);
     flags = laundromat(flags); //cleans the flags
 
+    let list = +args.listId
+    let entry = +args.entryId
+    let dest = +args.destId
+
     switch(cmd){
-        case 'move':
+        case 'move': 
+            if(validate(message, list, entry, dest)){
+                
+            }
             break;
         case 'add':
+            list = list ?? 1
+            if(validate(message, list)){
+                
+            }
             break;
         case 'delete':
+            if(validate(message, list, entry)){
+
+            }
             break;
         case 'feature':
+            if(validate(message, list, entry)){
+                
+            }
             break;
-        case 'lc':
+        case 'lc': 
+            if(validate(message, list, entry)){
+                
+            }
             break;
         case 'edit':
+            if(validate(message, list, entry)){
+                
+            }
             break;
         case 'list':
-            break;
+            if(validate(message, list)){
 
+            }
+            break;
 
         case 'random':
         case 'help':
