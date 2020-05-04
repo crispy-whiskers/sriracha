41e8372814b320ebc0d65af03bc86bc2e3ca8720
stinggy <stinggyray@gmail.com>
2020-05-04 06:42:46 -0500

    add public server updating

--- commands/add.js
+++ commands/add.js
@@ -1,9 +1,10 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-var Row = require('../row');
-var Discord = require('discord.js');
-var info = require('../config/globalinfo.json');
-var log = require('./log');
-var pFetch = require('./page');
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+var Row = require("../row");
+var Discord = require("discord.js");
+var info = require("../config/globalinfo.json");
+var log = require("./log");
+var pFetch = require("./page");
+var misc = require("./misc");
 
 /**
  * Secondhand function to accept flag object.
@@ -13,11 +14,11 @@ var pFetch = require('./page');
  * @param {*} flags
  */
 async function flagAdd(docs, message, flags) {
-	if (!flags.hasOwnProperty('l')) {
-		message.channel.send('Please provide a link with the `-l` flag!');
+	if (!flags.hasOwnProperty("l")) {
+		message.channel.send("Please provide a link with the `-l` flag!");
 	}
 
-	flags.l = flags.l.replace('http://', 'https://');
+	flags.l = flags.l.replace("http://", "https://");
 	let row = new Row(flags);
 	let list = flags?.s ?? 1;
 
@@ -34,10 +35,10 @@ async function flagAdd(docs, message, flags) {
 async function add(docs, message, list, row) {
 	await docs.loadInfo();
 
-	if(list <= 0 || list > info.sheetNames.length){
-        message.channel.send('Cannot add to a nonexistent sheet!')
-        return false;
-    }
+	if (list <= 0 || list > info.sheetNames.length) {
+		message.channel.send("Cannot add to a nonexistent sheet!");
+		return false;
+	}
 
 	if (list == 4) {
 		for (let x = 0; x < 3; x++) {
@@ -46,23 +47,54 @@ async function add(docs, message, list, row) {
 				if (row.page == -1) continue;
 				break;
 			} catch (e) {
-				await new Promise((resolve, reject)=>setTimeout(resolve, 500));
+				await new Promise((resolve, reject) => setTimeout(resolve, 500));
 			}
 		}
 		if (row.page == -1) {
-			message.channel.send('Failed to get page numbers! Please set it manually with `-pg`.');
+			message.channel.send("Failed to get page numbers! Please set it manually with `-pg`.");
 		}
 	}
 
 	try {
 		let sheet = docs.sheetsById[info.sheetIds[list]];
-		let r = await sheet.addRow(row.toArray());
-		await message.channel.send(`Successfully added \`${list}#${r.rowNumber - 1}\`!`);
+		let newRow = await sheet.addRow(row.toArray());
+		await message.channel.send(`Successfully added \`${list}#${newRow.rowNumber - 1}\`!`);
 
 		if (list == 4) {
-			misc.update();
+			await misc.update();
+
+			//update public server
+			let embed = new Discord.MessageEmbed();
+			embed.setColor("#FF0625");
+			embed.setTimestamp(new Date().toISOString());
+			embed.setURL(row.link);
+			embed.setTitle(row.title);
+			if (row.author) embed.setDescription("by " + row.author);
+			else embed.setDescription("No listed author");
+
+			embed.addField("Warnings", row.warning ?? "None", true);
+			embed.addField("Parody", row.parody ?? "None", true);
+			embed.addField("Tier", row.tier ?? "Not set", true);
+			embed.addField("Page#", row.page ?? "Not set", true);
+
+			var str = "";
+			if (row.tags?.length > 0) {
+				row.tags.forEach((e) => {
+					str += ` ${e},`;
+				});
+				str = str.replace("undefined", "");
+				str = str.substring(0, str.length - 1).trim();
+				embed.addField("Tags", str);
+			} else embed.addField("Tags", "None");
+			embed.setFooter("Wholesome God List");
+
+			log.updatePublicServer(embed);
+
+			let s = docs.sheetsById['' + info.sheetIds[7]];
+        	await s.addRow([row.link, row.author, row.tier, flags.l])
+			message.channel.send("Updated public server / website!");
 		}
-		
+
 		return true;
 	} catch (e) {
 		log.logError(message, e);
--- commands/log.js
+++ commands/log.js
@@ -8,6 +8,9 @@ module.exports.setup = function(dBot){
 module.exports.log = function (msg) {
     bot.channels.fetch(info.logs).then((channel) => channel.send(msg));
 }
+module.exports.updatePublicServer = function (embed) {
+    bot.channels.fetch(info.newListEntries).then((channel) => channel.send(embed));
+}
 module.exports.logError = function (message, error) {
     console.log('Error happened sent by ' + message?.author?.tag + ' with command: ' + message.content)
     console.log(error);
