fec8765efab680e0d8e97f9f57cdcfe24c682454
ladsfdsfs <nguyen.k.allen@gmail.com>
2021-06-18 10:02:48 -0400

    fix edge case on page fetching

--- commands/page.js
+++ commands/page.js
@@ -1,6 +1,9 @@
 var Row = require('../row');
 var axios = require('axios').default;
-var info = require('../config/globalinfo.json');
+const decode = require('html-entities').decode;
+const JSSoup = require('jssoup').default;
+
+//var info = require('../config/globalinfo.json');
 
 /**
  *
@@ -11,21 +14,31 @@ async function fetch(url) {
 	return new Promise((resolve, reject) => {
 		if (url.indexOf('nhentai') == -1 && url.indexOf('imgur') == -1) reject(-1); //not mainstream site. cannot fetch.
 		resolve(); //continue next link of promise chain immediately
-	}).then(() => {
+	}).then(async () => {
 		if (url.indexOf('nhentai') > -1) {
 
 			//ensure good url
 			if (!url.match(/^https:\/\/nhentai.net\/g\/\d+/)) reject(-1);
 			if (!url.match(/https:\/\/nhentai.net\/g\/\d+\//g)) url += '/';
 
-			return axios.get(url).then((resp) => {
-				//resp.data is a large string containing the entire webpage in html
-				code = +resp.data?.match(/\<span class="name"\>(\d+)</)[1] ?? -1;
-				//code = +resp.data?.match(/(\d+) pages/)[1] ?? -1; old nh ui
 
-				if (code == -1) throw code;
-				else return code;
-			});
+			let response = axios.get(url).then((resp) => {
+				return resp?.data ?? -1;
+			}); //if i truly was a good coder id put exp fallback here
+			response = await response;
+			if(response==-1) {reject(-1); return;}
+			let soup = new JSSoup(response);
+
+			let page = soup
+					.findAll('a', 'tag')
+					.filter((s) => {
+						return s?.attrs?.href?.match(/\/search\/(.*)/)
+					
+					})[0]
+					.find('span', 'name').text
+					
+			return page
+			
 		} else {
 			let hashCode = /https:\/\/imgur.com\/a\/([A-z0-9]*)/.exec(url)[1];
 			//extract identification part from the link
@@ -47,3 +60,10 @@ async function fetch(url) {
 	});
 }
 module.exports = fetch;
+
+fetch('https://nhentai.net/g/272353/').then(v=>{
+	console.log(v)
+})
+fetch('https://nhentai.net/g/222222/').then(v=>{
+	console.log(v)
+})
